c18d412fe7cfbcf3c2b05b3fbe15d3996291aa04
Peter Vrutneski <pvrutneski@gmail.com>
2022-06-02 09:26:29 -0600

    added js files to public, seeds , and util files

---
+++ public/js/comment.js
@@ -0,0 +1,31 @@
+const newCommentHandler = async (event) => {
+  event.preventDefault();
+
+  if (event.target.hasAttribute("data-id")) {
+    const blog_id = event.target.getAttribute("data-id");
+    const comment_text = document
+      .querySelector("#comment-content")
+      .value.trim();
+    console.log(comment_text, blog_id);
+
+    if (blog_id && comment_text) {
+      const response = await fetch(`/api/comments`, {
+        method: "POST",
+        body: JSON.stringify({ blog_id, comment_text }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        document.location.replace(`/blog/${blog_id}`);
+      } else {
+        alert("Failed to create comment");
+      }
+    }
+  }
+};
+
+document
+  .querySelector(".submit-button")
+  .addEventListener("click", newCommentHandler);
